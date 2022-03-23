dc3524f6e2f312e948439ce469d96ef6c137cf4c
Pradheep Rajendirane <pas495@gmail.com>
2022-03-23 14:45:44 +0100

    add line breaks

--- client/src/routes/Message/Chatbox/MessageItem.tsx
+++ client/src/routes/Message/Chatbox/MessageItem.tsx
@@ -5,12 +5,12 @@ type Props = {
   message: IMessage
 }
 
-function MessageItem({selectedPhone, message}: Props) {
+function MessageItem({ selectedPhone, message }: Props) {
 
   return (
     <div className={'chat-msg-container' + (selectedPhone.phone_id === message.to_phone_id ? '' : ' right')}>
       <div className="chat-text-box">
-        <p>{message.body}</p>
+        <p style={{ 'whiteSpace': 'pre-line' }}>{message.body}</p>
         <span className="chat-timestamp">{message.created_on.toLocaleDateString()} at {message.created_on.toLocaleTimeString()}</span>
       </div>
     </div>
